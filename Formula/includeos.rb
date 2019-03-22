661d49ac2bf373210455f236b1232a12c2d32c19
Magnus Skjegstad <magnus@skjegstad.com>
2019-03-22 10:57:03 +0100

    Test revision bump
    Signed-off-by: Magnus Skjegstad <magnus@skjegstad.com>

--- Formula/includeos.rb
+++ Formula/includeos.rb
@@ -6,7 +6,7 @@ class Includeos < Formula
 	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
 	version "0.1.0"
-	revision 1
+	revision 2
 	
 	depends_on "python3"
 	depends_on "conan"
